600420978f2174ac3ddc15452895c145e956faa9
test <test@test>
2026-03-05 17:09:31 +0800

    add utils and nav
    nav.css|3.1|TST||add utils and nav
    utils.js|3.1|TST||add utils and nav

---
+++ utils.js
@@ -0,0 +1,2 @@
+function debounce(fn,delay){var timer=null;return function(){var args=arguments;var context=this;clearTimeout(timer);timer=setTimeout(function(){fn.apply(context,args);},delay);};}
+function formatDate(date){var d=new Date(date);return d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();}
